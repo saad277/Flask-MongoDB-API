482e83c30dd02820a19dcb7133612a2a8e185e8b
Saad Sheikh <saadsheikh27@gmail.com>
2020-09-21 11:06:53 +0500

    POST

---
+++ __init__.py
@@ -0,0 +1,31 @@
+import os
+import json
+import datetime
+from bson import json_util
+from bson.objectid import ObjectId
+from flask import Flask,request,jsonify
+from flask_pymongo import pymongo
+
+
+app = Flask(__name__)
+
+CONNECTION_STRING = "mongodb+srv://saad:features@cluster0.hdo8c.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
+
+
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('MongoExample1')
+user_collection = pymongo.collection.Collection(db,'hello')
+
+
+
+#test to insert data to the data base
+@app.route("/test")
+def test():
+    db.collection.insert_one({"name": "Johaaan"})
+    return ("Connected to the data base!")
+        
+
+
+app.run(debug=True,port=80)
+
